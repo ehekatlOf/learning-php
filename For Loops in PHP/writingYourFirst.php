0adbf52d1570ea5df6b159efb48ed76523425a37
ehekatlOf <pranagy@gmail.com>
2016-09-11 22:53:56 +0000

    For Loops

---
+++ For Loops in PHP/writingYourFirst.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Solo For Loop!</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      for ($i = 10; $i<110; $i = $i+10){
+          echo $i;
+      }
+      ?>
+    </p>
+  </body>
+</html>
