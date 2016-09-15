78ebbdd94e1d2af8b7ef8928782dcb1c86c87bea
ehekatlOf <pranagy@gmail.com>
2016-09-15 07:06:06 +0000

    functions 2

---
+++ Functions 2/functionRefresher.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+            echo strlen("abc");
+        ?>
+      </p>
+    </body>
+</html>
---
+++ Functions 2/yourFirstFunction.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        // Write your function below!
+        
+        function displayName(){
+            echo "maybe";
+        };
+        
+        displayName();
+        ?>
+      </p>
+    </body>
+</html>
